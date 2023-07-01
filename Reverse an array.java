9b3feb8ddaa7974b4a4e95706bca97851ac36257
Saurav Raj <118835139+Saurav-rajk@users.noreply.github.com>
2023-07-01 09:33:26 +0530

    Add files via upload

---
+++ Reverse an array.java
@@ -0,0 +1,10 @@
+//Java program to reverse an array of integer values.
+public class Main{
+    public static void main(String[] args){
+        int[]arr1={3,5,7,8};
+        for(int i=arr1.length-1;i>=0;i--){
+            System.out.print(arr1[i]+" ");
+        }
+    }
+
+}
